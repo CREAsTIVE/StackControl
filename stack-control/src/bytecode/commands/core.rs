b74106e9a29b60daa9c210b571583557d8d54059
CREAsTIVE <alexshveev@gmail.com>
2025-05-22 20:00:56 +0500

    refactor: unused removed

--- stack-control/src/bytecode/commands/core.rs
+++ stack-control/src/bytecode/commands/core.rs
@@ -1,7 +1,6 @@
-use indoc::indoc;
 use crate::{compiletime::command_map::CommandMap, runtime::{stack::Stack, value::{Array, Value}}};
 
-use super::{math::append_math, stack_manipulators::append_stack_manipulators, CommandExecutable, CommandMeta, DescribedCommand, DescribedCommandMaker, RuntimeException};
+use super::{math::append_math, stack_manipulators::append_stack_manipulators, CommandExecutable, DescribedCommand, RuntimeException};
 
 pub fn bind_default_commands(map: &mut CommandMap) {
   append_stack_manipulators(map);
@@ -12,63 +11,6 @@ fn _test(_stack: &mut Stack) -> Result<(), RuntimeException> {
       //
       Ok(())
 }
-
-/*
-macro_rules! define_commands {
-  (define $group:ident $(($metadata:tt $stack:ident $defenitions:block)),+) => {
-    define_commands!{defenition $(($metadata $stack $defenitions)),+}
-    pub fn $group(cmd_map: &mut $crate::compiletime::command_map::CommandMap) {
-      define_commands!{meta cmd_map $(($metadata $stack $defenitions)),+}
-    }
-  };
-
-  // With additional fields
-  (meta $command_map:ident ([$name:ident $key:tt [$($alias:expr),*] {$($vkey:ident : $value:expr),+}] $stack:ident $defenition:block)) => {
-    $command_map.set($crate::bytecode::commands::DescribedCommand {
-      execution: Box::new($name {}),
-      meta: std::sync::Arc::new($crate::bytecode::commands::CommandMeta {
-        key: String::from($key),
-        aliases: [$($alias),*].iter().map(|s: &&str| s.to_string()).collect::<Vec<String>>(),
-        $($vkey : $value),+,
-        ..core::default::Default::default()
-      })
-    });
-  };
-
-  // No additional fields
-  (meta $command_map:ident ([$name:ident $key:tt [$($alias:expr),*] {}] $stack:ident $defenition:block)) => {
-    $command_map.set($crate::bytecode::commands::DescribedCommand {
-      execution: Box::new($name {}),
-      meta: std::sync::Arc::new($crate::bytecode::command::CommandMeta {
-        key: $key,
-        aliases: [$($alias),*].iter().map(|s: &&str| s.to_string()).collect::<Vec<String>>(),
-        ..core::default::Default::default()
-      })
-    });
-  };
-
-  (meta $command_map:ident ($metadata:tt $stack:ident $defenition:block), $(($metadatas:tt $stacks:ident $defenitions:block)),+) => {{
-    define_commands!{meta $command_map ($metadata $stack $defenition)}
-    define_commands!{meta $command_map $(($metadatas $stacks $defenitions)),+}
-  }};
-
-  (defenition ([$name:ident $key:tt [$($alias:expr),*] $others:tt] $stack:ident $defenition:block)) => {
-    pub struct $name {}
-    impl $crate::bytecode::commands::CommandExecutable for $name {
-      fn execute(&self, $stack: &mut $crate::runtime::stack::Stack) -> Result<(), $crate::bytecode::commands::RuntimeException> $defenition
-
-      fn to_string(&self) -> String {
-        String::from($key)
-      }
-    }
-  };
-
-  (defenition ($metadata:tt $stack:ident $defenition:block), $(($metadatas:tt $stacks:ident $defenitions:block)),+) => {
-    define_commands!{defenition ($metadata $stack $defenition)}
-    define_commands!{defenition $(($metadatas $stacks $defenitions)),+}
-  };
-}
-*/
 
 macro_rules! define_commands {
   ($group:ident $($name:ident $args:tt $body:tt),*) => {
